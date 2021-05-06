3537424a58d86c08abf3a0b7b3dcc979d2ee93cc
aahnik <daw@aahnik.dev>
2021-05-06 12:22:31 +0530

    get version dynamically, instead of hardcoding

--- telewater/__init__.py
+++ telewater/__init__.py
@@ -1,8 +1,8 @@
 ''' A telegram bot that applies watermark on images, gifs and videos. '''
 
-
+from importlib.metadata import version
 from telewater.main import start_bot
 
-__version__ = "0.0.7"
+__version__ = version(__package__)
 
 # AAHNIK 2021
