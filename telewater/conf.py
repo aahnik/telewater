dd1c87480bf7c3f0fdcf1dfeb04194ddc60cf204
Mahesh1414 <89467357+Mahesh1414@users.noreply.github.com>
2021-09-03 22:56:34 +0530

    Update conf.py

--- telewater/conf.py
+++ telewater/conf.py
@@ -8,7 +8,7 @@ class Config(BaseModel):
     watermark: str = "https://raw.githubusercontent.com/Mahesh1414/image/main/20210903_221413.png"
     frame_rate: int = 15
     preset: str = "ultrafast"
-    position: Position = Position.topright
+    position: Position = Position.center
 
     @validator("preset")
     def validate_preset(val):
